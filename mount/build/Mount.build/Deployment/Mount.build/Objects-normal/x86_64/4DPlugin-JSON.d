6b4cba03119754117de7757929fabc0090fa315e
miyako <keisuke.miyako@4d.com>
2019-10-16 18:14:09 +0900

    notarise

--- mount/build/Mount.build/Deployment/Mount.build/Objects-normal/x86_64/4DPlugin-JSON.d
+++
@@ -1,6 +0,0 @@
-dependencies: \
-  /Users/miyako/miyako@github.com/4d-plugin-mount/mount/support/4DPlugin-JSON.cpp \
-  /Users/miyako/miyako@github.com/4d-plugin-mount/mount/support/4DPlugin-JSON.h \
-  /Users/miyako/miyako@github.com/4d-plugin-mount/mount/4D\ Plugin\ API/4DPluginAPI.h \
-  /Users/miyako/miyako@github.com/4d-plugin-mount/mount/4D\ Plugin\ API/Flags.h \
-  /Users/miyako/miyako@github.com/4d-plugin-mount/mount/4D\ Plugin\ API/PublicTypes.h
